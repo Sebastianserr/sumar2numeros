5048a36fadb286f2c86417bd7b6e9b62708b4d52
sebitas <sebastianserrano9597@gmaol.com>
2023-09-20 09:21:50 -0500

    primer commit

---
+++ src/clase308/Clase308sebas.java
@@ -0,0 +1,29 @@
+
+package clase308;
+import java.util.Scanner;
+public class Clase308sebas {
+
+    
+    public static void main(String[] args) {
+        Scanner leer=new Scanner(System.in);
+        Float nu1,nu2,nu3,nu4,nu5,r;
+        System.out.print("ingresa el primer numero: ");
+        nu1=leer.nextFloat();
+        System.out.print ("ingresa el segundo numero: ");
+        nu2=leer.nextFloat();
+        System.out.print("ingresa l tercer numero: ");
+        nu3=leer.nextFloat();
+        System.out.print("ingresa el cuarto numero: ");
+        nu4=leer.nextFloat();
+        System.out.print("ingresa el quinto numero: ");
+        nu5=leer.nextFloat();
+        r=nu1+nu2+nu3+nu4+nu5;
+        System.out.print("la suma es :"+r);
+    }
+    
+}
+
+        
+    
+    
+
